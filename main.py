4896ecc03e9b80bd6edc63b00ec781a33b0914d3
Gav1012 <gavin.poley@gmail.com>
2024-01-20 23:54:38 -0800

    added some imports and functions

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Welcome to Gmail Project")
+import base64
+import os.path
+import pickle
+import json
+
+def authenticate_gmail():
+    return
+
+def create_gmail_cred():
+    return
+
+def read_emails():
+    return
+
+
+if __name__ == "__main__":
+    read_emails()
